c2d2dd12c727c13891b6a327c2f7344d85a5c07b
keegan-h <keeganslane@gmail.com>
2019-04-22 08:31:41 -0700

    fresh data

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ let myChart = document.getElementById("myChart").getContext("2d");
 let massPopChart = new Chart(myChart, {
     type: 'bar', // other types of charts available: bar, horizontalBar, pie, line, doughnut, radar, polarArea
     data: {
-        labels: ["Seattle", "Portland", "Vancouver", "San Francisco", "Los Angeles"],
+        labels: ["Los Angeles", "San Diego", "San Jose", "San Francisco", "Seattle", "Portland", "Fresno", "Sacramento", "Longbeach", "Oakland"],
         datasets: [{
             label: "Population",
             data: [
+                3999759,
+                1419516,
+                1035317,
+                884363,
                 724745,
                 647805,
-                2555884,
-                884363,
-                3999759
+                527438,
+                501901,
+                469450,
+                425195
+
             ],
             backgroundColor: [
                 '#6ba1ff',
