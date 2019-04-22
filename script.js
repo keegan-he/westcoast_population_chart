03a435ad52a934a2ad55e3056758147879bd4240
Keegan H <keeganslane@gmail.com>
2019-04-21 23:04:19 -0700

    added portland

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let myChart = document.getElementById("myChart").getContext("2d");
 let massPopChart = new Chart(myChart, {
     type: 'bar', // other types of charts available: bar, horizontalBar, pie, line, doughnut, radar, polarArea
     data:{
-        labels:["Seattle", "Vancouver", "San Francisco", "Los Angeles"],
+        labels:["Seattle", "Portland", "Vancouver", "San Francisco", "Los Angeles"],
         datasets:[{
             label: "Population",
             data:[
             724745,
+            4256350,
             2555884,
             884363,
             3999759
